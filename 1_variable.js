6f216576decd7af97c55f7c150aeb527a9c407a8
Jeet Prajapati <jeet.p@crestskillserve.com>
2024-12-18 16:23:06 +0530

    variable and datatypes

---
+++ 1_variable.js
@@ -0,0 +1,14 @@
+const accountId =1234
+let email = "jeet.p@crestskillserve.com"
+var password = "Jeet@123"
+city = "surat"
+
+// accountId = 1 //can't change
+
+// console.log(accountId);
+
+email ="xyz"
+password = "123"
+city = "anjar"
+
+console.table([accountId, email, password, city])
---
+++ 2_datatypes.js
@@ -0,0 +1,6 @@
+console.log(typeof "Jeet")
+console.log(typeof 3)
+console.log(typeof true)
+console.log(typeof 2.3)
+console.log(typeof null)
+console.log(typeof undefined)
