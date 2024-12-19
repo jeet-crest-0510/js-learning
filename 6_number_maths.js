6cb6858a199892cfafb7f9a97edc2d2aca4a5795
Jeet Prajapati <jeet.p@crestskillserve.com>
2024-12-19 10:58:10 +0530

    Add Number and Math

---
+++ 6_number_maths.js
@@ -0,0 +1,16 @@
+const num = new Number(10000.49)
+
+// console.log(num);
+
+// console.log(num.toFixed(2));
+// console.log(num.toPrecision(6));
+// console.log(num.toLocaleString('en-In'));
+
+console.log(Math.abs(-5))
+console.log(Math.round(-5.55))
+console.log(Math.min(-5, 4,7))
+
+const min=1, max=6
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
