51bb391f75ae02d2478bb561bf1374e1cd987771
Jeet Prajapati <jeet.p@crestskillserve.com>
2024-12-19 17:23:16 +0530

    Add Functions and Scopes

---
+++ 2_basics/0_functions.js
@@ -0,0 +1,55 @@
+// function add(num1, num2){
+//     return num1+num2
+// }
+
+// console.log(add(2, 3));  
+// console.log(add(2, "2"));
+// console.log(add(2 , null));
+
+// console.log(add())
+
+// if(!""){
+//     console.log("Hello1");
+// }
+
+// if(!undefined){
+//     console.log("Hello2");
+// }
+
+// if(!null){
+//     console.log("Hello3");
+// }
+
+//default value
+// function login(name = "Jeet"){
+//     return `${name} Hello!`
+// }
+
+// console.log(login());
+// console.log(login("Jay"));
+
+// user = {
+//     id: "abc",
+//     name: "Jeet"
+// }
+
+// function display(...num){
+//     console.log(num);
+//     console.log(typeof num);
+    
+// }
+
+// display(1,2,3,4,5, user)
+
+
+
+// function display(obj){
+//     console.log(obj.name);
+// }
+
+// display(user)
+// display({
+//     id:"xyz",
+//     name: "jay"
+// })
+
---
+++ 2_basics/1_scope.js
@@ -0,0 +1,84 @@
+// let a =10
+// var b = 20
+// const c= 30
+
+// if(true){
+//     let a = 100
+//     var b = 200
+//     console.log(a,b,c);
+    
+// }
+
+// console.log(a,b,c);
+
+// console.log(add(5));
+
+// function add(n){
+//     return n+1
+// }
+
+// const add = function(n){
+//     return n+1
+// }
+
+// console.log(add(5));
+
+
+//This in documents
+
+// const user = {
+//     name: "jeet",
+//     message: function(){
+//         console.log(`Hello ${this.name}`);
+//         console.log(this);
+//     }
+// }
+
+// user.message()
+// user.name = "Jay"
+// user.message()
+
+// console.log(this);
+
+// function x(){
+//     let y = 1
+//     console.log(this.y);
+    
+// }
+
+// x()
+
+//Arroe Function
+//Explicit
+
+// const add = (n1,n2) => {
+//     return n1+n2
+// }
+
+// console.log(add(1, 2));
+
+
+//Implicit
+// const add = (n1,n2) => (n1+n2)
+// console.log(add(1, 2));
+
+// const display = (name) => ({name: "Jeet"})
+// console.log(display());
+
+//IIFE
+//Named
+
+// (function add(num1, num2){
+//     console.log(num1+num2);
+//     // return num1+num2
+// })(1,2)
+
+//UnNamed
+
+// (
+//     (n1,n2) => {
+//         console.log(n1+n2);
+//     }
+// )(1,2)
+
+
